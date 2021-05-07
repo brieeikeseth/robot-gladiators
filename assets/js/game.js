7a014dbd6db19415ce6f3eca4a45c7371d33d1a1
Brie Eikeseth <brieeikeseth56@gmail.com>
2021-05-07 13:00:15 -0400

    3.1.5

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+
+// fight(); 
+
+window.alert("The fight has begun!");
+
+var playerName = window.prompt("What is your robot's name?");
+// this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
